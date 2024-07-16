353bbcd8ce0617afd87339aa201529274f11000d
drunken_neoguri <developneoguri@gmail.com>
2024-07-16 22:36:08 +0900

    [Enhancement] Splash Screen 패키지 설치 후 Splash 스크린 배치

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BootSplash from 'react-native-bootsplash';
 import MainScreen from '../screens/main';
 
 export const Stack = createNativeStackNavigator();
 
 export default function MainStack() {
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={() => BootSplash.hide({fade: true})}>
       <Stack.Navigator initialRouteName="main">
         <Stack.Screen
           name="main"
